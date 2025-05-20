1668a46716c00e558c5d73106e08215283af2cd5
SSeries <63185402+S-Series@users.noreply.github.com>
2025-05-20 13:10:58 +0900

    Backend Edit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, UploadFile, File, Query
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-import easyocr
+
 from PIL import Image
+from typing import List, Optional
+
+import easyocr
 import io
 import numpy as np
 
@@ -11,6 +16,7 @@ origins = [
 ]
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="assets"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -62,3 +68,63 @@ async def perform_ocr(
         return {"error": f"OCR failed: {str(e)}"}
 
     return {"text": result}
+
+@app.post("/profile")
+async def profile_img(
+    key: str = Query(default="NaN"),
+    lang: str = Query("en", enum=["en", "kr", "ja", "cn"]),
+
+    weapon_value: List[str] = Query(default=["","","","",""]),
+    exho_value: List[str] = Query(default=["","","","",""]),
+    stats: List[float] = Query(default=[[],[],[],[],[],[]]),
+    stats_icon: List[float] = Query(default=[[],[],[],[],[],[]]),
+    score_value: List[float] = Query(default=[0.0, 0.0]),
+
+    img: Optional[UploadFile] = File(None),
+    value: Optional[List[float]] = Query(default=[0, 0, 1]),
+):
+    # 고정변수 설정
+    image_pos = [] 
+    image_paths = []
+    canvas_width, canvas_height = 2520 + 32, 1080 + 32
+    canvas = Image.new("RGBA", (canvas_width, canvas_height), (0, 0, 0, 0))
+
+    if True: #key is Available:
+    # 받은 이미지가 있는지 확인
+        if img is not None:
+            base_img = Image.open(img.file).convert("RGBA")
+        else:
+            base_img = Image.open("default.png").convert("RGBA")  # 기본 이미지
+    else:
+        base_img = Image.open("default.png").convert("RGBA")  # 기본 이미지
+    
+    # 스케일 값 확인 (유저설정값 * 컨테이너 사이즈 보정)
+    scale = value[2]
+    if scale != 1:
+        new_size = (int(base_img.width * scale), int(base_img.height * scale))
+        base_img = base_img.resize(new_size)
+
+    # 배경 이미지 중첩그리기
+    paste_x, paste_y = int(value[0]), int(value[1])
+    temp_layer = Image.new("RGBA", (canvas_width, canvas_height), (0, 0, 0, 0))
+    temp_layer.paste(base_img, (paste_x, paste_y))
+
+    # 고정 이미지들 그려넣기
+    for i, img_path in enumerate(image_paths):
+        img_layer = Image.open(img_path).convert("RGBA")
+        pos = image_pos[i]
+
+        temp_layer = Image.new("RGBA", (canvas_width, canvas_height), (0, 0, 0, 0))
+        temp_layer.paste(img_layer, pos)
+        canvas = Image.alpha_composite(canvas, temp_layer)
+
+    # 장비 이미지 붙여넣기
+    img_layer = Image.open("https://www.wwaves.dev/weapon/{weapon_value[0]}.png").convert("RGBA")
+    temp_layer = Image.new("RGBA", (canvas_width, canvas_height), (0, 0, 0, 0))
+    temp_layer.paste(img_layer, pos)
+    canvas = Image.alpha_composite(canvas, temp_layer)
+
+    buf = io.BytesIO()
+    canvas.save(buf, format="PNG")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
